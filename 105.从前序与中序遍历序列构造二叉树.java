9e5b050b63c0e1985842563a592dce4e6c04c7df
luser233 <2854697833@qq.com>
2020-12-10 21:48:55 +0800

    second

--- 105.从前序与中序遍历序列构造二叉树.java
+++ 105.从前序与中序遍历序列构造二叉树.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * @lc app=leetcode.cn id=105 lang=java
  *
@@ -5,19 +8,33 @@
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
 
+//   Definition for a binary tree node.
+  
+ 
+class Solution {
+     class TreeNode {
+        int val;
+       TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
     }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+Map<Integer,Integer> inMap=new HashMap<Integer,Integer>();
+   for (int i = 0; i < inorder.length; i++) {
+       inMap.put(inorder[i], i);
+   }
+   TreeNode root=buildTree(preorder, 0,preorder.length-1,inorder,0,inorder.length-1,inMap);
+   return root;
+
+
+}
+public TreeNode  buildNode(int[] preorder,int prestart,int preend,int [] inorder,int instart,int inend,Map<Integer, Integer>) {
+    
+}
+
+
+
 }
 // @lc code=end
 
