3bbdf0aba7c1f9f661d01184f9d4d91a21c54aa6
luser233 <2854697833@qq.com>
2020-12-10 22:38:08 +0800

    第一种方法

--- 105.从前序与中序遍历序列构造二叉树.java
+++ 105.从前序与中序遍历序列构造二叉树.java
@@ -10,32 +10,37 @@ import java.util.Map;
 // @lc code=start
 
 //   Definition for a binary tree node.
-  
+// class TreeNode {
+//     int val;
+//    TreeNode left;
+//     TreeNode right;
+//     TreeNode(int x) { val = x; }
+// }
  
 class Solution {
-     class TreeNode {
-        int val;
-       TreeNode left;
-        TreeNode right;
-        TreeNode(int x) { val = x; }
-    }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-Map<Integer,Integer> inMap=new HashMap<Integer,Integer>();
-   for (int i = 0; i < inorder.length; i++) {
-       inMap.put(inorder[i], i);
-   }
-   TreeNode root=buildTree(preorder, 0,preorder.length-1,inorder,0,inorder.length-1,inMap);
-   return root;
-
-
-}
-public TreeNode  buildNode(int[] preorder,int prestart,int preend,int [] inorder,int instart,int inend,Map<Integer, Integer>) {
-    if (preorder>preend||instart>inend) {
-        return null;
+        Map<Integer, Integer> inMap = new HashMap<Integer, Integer>();
+        
+        for(int i = 0; i < inorder.length; i++) {
+            inMap.put(inorder[i], i);
+        }
+    
+        TreeNode root = buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inMap);
+        return root;
     }
-    TreeNode root=new tree
-}
-
+    
+    public TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inMap) {
+        if(preStart > preEnd || inStart > inEnd) return null;
+        
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int inRoot = inMap.get(root.val);
+        int numsLeft = inRoot - inStart;
+        
+        root.left = buildTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, inMap);
+        root.right = buildTree(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, inMap);
+        
+        return root;
+    }
 
 
 }
