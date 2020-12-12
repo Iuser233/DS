909ac840fe1903fb6d1bb1ab8d0b4867b9f47625
luser233 <2854697833@qq.com>
2020-12-12 20:31:39 +0800

    aa

--- Graph.java
+++ Graph.java
@@ -2,7 +2,14 @@ public   class Graph {
       final int MaxSize=10;//邻接矩阵可以表示的最大顶点数
      final int MAX=999;//在网中，表示没有联系
      int [] V;//顶点信息
+int[][]arcs;//邻接矩阵
+int e;//边数
+protected int n;//定点数
+public Graph(){
+      V=new int[10];//默认十个顶点
+      arcs
 
+}
     
 
 }
